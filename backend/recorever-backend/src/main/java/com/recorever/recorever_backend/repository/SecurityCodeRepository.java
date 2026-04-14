29444ad2f894db4493f44181fb9e77bc661e6296
Riomalos-ZyrrahFeil <zriomalos@gmail.com>
2026-04-15 01:00:39 +0800

    feat(backend-email_verification): implement SecurityCodeRepository for token retrieval

---
+++ backend/recorever-backend/src/main/java/com/recorever/recorever_backend/repository/SecurityCodeRepository.java
@@ -0,0 +1,9 @@
+package com.recorever.recorever_backend.repository;
+
+import com.recorever.recorever_backend.model.SecurityCode;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface SecurityCodeRepository extends JpaRepository<SecurityCode, Integer> {
+    Optional<SecurityCode> findByVerificationToken(String token);
+}
