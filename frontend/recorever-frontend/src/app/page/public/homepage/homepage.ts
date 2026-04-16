f6c278fe4539e0d72d54869db414d66553571ca5
jallaine <jallainesiervo143@gmail.com>
2026-04-16 10:23:06 +0800

    feat(frontend/homepage): implement "How It Works" section FAQ accordion and step-by-step guide

--- frontend/recorever-frontend/src/app/page/public/homepage/homepage.ts
+++ frontend/recorever-frontend/src/app/page/public/homepage/homepage.ts
@@ -10,6 +10,13 @@ type Feature = {
   details: string;
 };
 
+type FaqItem = {
+  question: string;
+  answer?: string;
+  steps?: string[];
+  isOpen: boolean;
+};
+
 @Component({
   selector: 'app-homepage',
   standalone: true,
@@ -57,8 +64,56 @@ export class Homepage {
     }
   ];
 
+  public faqs: FaqItem[] = [
+    {
+      question: 'What should I do if I lost an item?',
+      steps: [
+        'Browse Found Items to see if someone has already turned it in.',
+            'Claim Your Item: If you spot your item, click the ' +
+            '"Claim Item" button of the item.',
+        'Save Your Reference Code.',
+        'Visit the Administrator: Once you have your Reference Code, ' +
+            'take it to the designated administrator\'s office.',
+        'If Not Found In Browsing: Report Lost Item. The system will instantly alert you when if ' +
+            'someone reports a found item that matches your description'
+      ],
+      isOpen: false
+    },
+    {
+      question: 'What should I do if I found an item?',
+      steps: [
+        'Report the Item: Provide required informations in the form.',
+        'Save Your Reference Code.',
+        'Turn It In: Bring the found item and your Reference Code ' +
+            'directly to the designated administrator\'s office.',
+        'Verification & Posting: Once approved, your report will be made ' +
+            'public for all users to view, and our Automated Smart Matching ' + 
+            'will instantly notify the owner if they are actively looking for it!'
+      ],
+      isOpen: false
+    },
+    {
+      question: 'How does the Secure Claim Verification work?',
+      answer: 'To claim an item, you must provide verifiable proof of '+ 
+          'ownership, such as an ID, a detailed description of ' + 
+          'unique features, or a password if applicable. Your data is kept entirely private.',
+      isOpen: false
+    },
+    {
+      question: 'How long are found items kept in the system?',
+      answer: 'Found items are securely stored for 30 days. After reaching' + 
+          ' this retention limit, our system sends automated aging ' + 
+          'notifications to the administration for appropriate turnover or donation.',
+      isOpen: false
+    }
+  ];
+
   public get carouselItems(): Feature[] {
     return [...this.features, ...this.features];
+  }
+
+  public toggleFaq(index: number): void {
+    this.faqs[index].isOpen = !this.faqs[index].isOpen;
   }
 
   navigateToLost(): void {
