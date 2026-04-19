3cdb7f2b39184ed16c9d2040de94dc480867fdd0
durante_stephanie <durantestephanie07@gmail.com>
2026-04-19 22:23:17 +0800

    feat(frontend-my-reports-page): implement My Reports page with search and filtering functionality

---
+++ frontend/recorever-frontend/src/app/page/shared-page/my-reports-page/my-reports-page.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MyReportsPage } from './my-reports-page';
+
+describe('MyReportsPage', () => {
+  let component: MyReportsPage;
+  let fixture: ComponentFixture<MyReportsPage>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MyReportsPage]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(MyReportsPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
