936772275193cf8b5503e22dde6b6f887b386be6
florido_maydelyn <maydelynflorido07@gmail.com>
2026-04-25 23:38:29 +0800

    feat(backend-dto): add new attributes to ReportCreationDTO and ReportResponseDTO

--- backend/recorever-backend/src/main/java/com/recorever/recorever_backend/dto/ReportResponseDTO.java
+++ backend/recorever-backend/src/main/java/com/recorever/recorever_backend/dto/ReportResponseDTO.java
@@ -1,6 +1,8 @@
 package com.recorever.recorever_backend.dto;
 
 import lombok.Data;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 /**
@@ -11,18 +13,23 @@ import java.util.List;
 public class ReportResponseDTO {
     private int report_id;
     private int user_id;
-    private String type; 
+    private String type;
+    private String category_name;
     private String item_name;
     private String location;
+    private String surrendered_location_name;
     private String date_lost_found;
     private String date_reported;
     private String date_resolved;
     private String description;
-    private String status; 
-    
+    private String status_name;
+
     private String expiry_date;
     private String surrender_code;
     private String reporter_name;
     private String reporter_profile_picture;
+
+    private LocalDateTime updatedAt;
+    private boolean isAdminEdit;
     private List<ImageResponseDTO> images;
 }
