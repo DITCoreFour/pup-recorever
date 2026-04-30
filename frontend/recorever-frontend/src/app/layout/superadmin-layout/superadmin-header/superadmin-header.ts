6ac449b59ce6afdaa9081f7740e65a30e840d4f9
Riomalos-ZyrrahFeil <zriomalos@gmail.com>
2026-04-30 18:33:40 +0800

    chore(frontend-header): add new superadmin-header component

---
+++ frontend/recorever-frontend/src/app/layout/superadmin-layout/superadmin-header/superadmin-header.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-superadmin-header',
+  imports: [],
+  templateUrl: './superadmin-header.html',
+  styleUrl: './superadmin-header.scss',
+})
+export class SuperadminLayout {
+
+}
